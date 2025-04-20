6684d059ba288b750bd797b64ed367671f6820bb
enric <enric.mendez-codina@epitech.eu>
2025-04-20 20:30:14 +0200

    ERASE

--- CubeState.cpp
+++
@@ -1,8 +0,0 @@
-#include "CubeState.hpp"
-#include <iostream>
-
-void SharedCubeState::printState() {
-    std::cout << "Player ID: " << player_id << std::endl;
-    std::cout << "Player 0: (" << players[0].x << ", " << players[0].y << ") Active: " << players[0].active << std::endl;
-    std::cout << "Player 1: (" << players[1].x << ", " << players[1].y << ") Active: " << players[1].active << std::endl;
-}
--- NetworkThread.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "CubeState.hpp"
-
-void run_network_thread(SharedCubeState* state, const char* ip, int port);
-void client_position(SharedCubeState *state, int sock);
